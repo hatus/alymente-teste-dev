adaf1e30e0c60ab95a9e06bc6be07dd5419c59ef
Hatus Níwman <hatusn@gmail.com>
2024-05-19 19:34:44 -0300

    feat: create controllers routes

--- src/http/controllers/persons/routes.ts
+++ src/http/controllers/persons/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
 import { createPersonController } from './create-person'
+import { deletePersonController } from './delete-person'
+import { getPersonController } from './get-person'
+import { listAllPersonsController } from './list-all'
+import { updatePersonController } from './update-person'
 
 export async function personsRoutes(app: FastifyInstance) {
   app.post('/', createPersonController)
+  app.get('/:userId', getPersonController)
+  app.delete('/:userId', deletePersonController)
+  app.get('/', listAllPersonsController)
+  app.put('/:userId', updatePersonController)
 }
