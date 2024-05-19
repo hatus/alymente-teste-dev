8d1dbe73fed3c836474b3d68f983820e38d39768
Hatus Níwman <hatusn@gmail.com>
2024-05-19 03:32:41 -0300

    feat: register persons routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
 
+import { personsRoutes } from './http/controllers/persons/routes'
 import { fastifyErrorHandler } from './libs/fastify-error-handler'
 
 const app = fastify()
+
+app.register(personsRoutes, { prefix: 'persons' })
 
 app.setErrorHandler(fastifyErrorHandler)
 
