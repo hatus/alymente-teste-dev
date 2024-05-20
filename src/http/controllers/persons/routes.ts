31ee5c43f9c25af82b230287a0050e0486adbca9
Hatus Níwman <hatusn@gmail.com>
2024-05-19 23:05:08 -0300

    feat: add route of import persons

--- src/http/controllers/persons/routes.ts
+++ src/http/controllers/persons/routes.ts
@@ -1,12 +1,20 @@
 import { FastifyInstance } from 'fastify'
 
+import { upload } from '@/libs/upload'
+
 import { createPersonController } from './create-person'
 import { deletePersonController } from './delete-person'
 import { getPersonController } from './get-person'
+import { importPersonsController } from './import-persons'
 import { listAllPersonsController } from './list-all'
 import { updatePersonController } from './update-person'
 
 export async function personsRoutes(app: FastifyInstance) {
+  app.post(
+    '/import',
+    { preHandler: [upload.single('file')] },
+    importPersonsController,
+  )
   app.post('/', createPersonController)
   app.get('/:userId', getPersonController)
   app.delete('/:userId', deletePersonController)
